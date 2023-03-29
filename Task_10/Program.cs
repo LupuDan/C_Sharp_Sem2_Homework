69c76fa8278280a9f2c22396ed39de6b2e21a0dd
Lupu Daniel <dlupu666@gmail.com>
2023-03-29 19:38:32 +0300

    Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 10: Напишите программу,
+//которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите трехзначное число: ");
+        int number = int.Parse(Console.ReadLine());
+        int second_digit = (number / 10) % 10;
+        Console.WriteLine("Вторая цифра введенного числа: " + second_digit);
+    }
+}
