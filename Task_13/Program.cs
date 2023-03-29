7fead57e8e6dfd1ad1e8ff1f0257005266bfab7e
Lupu Daniel <dlupu666@gmail.com>
2023-03-29 19:41:41 +0300

    task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        // Вычисляем третью цифру числа
+        int third_digit = (number / 100) % 10;
+
+        // Проверяем, есть ли третья цифра
+        if (third_digit != 0)
+        {
+            Console.WriteLine("Третья цифра введенного числа: " + third_digit);
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры введенного числа нет.");
+        }
+    }
+}
